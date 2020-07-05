a3a0cff4f9fec47dc3afd58edac098f0c46f6be9
TaeWon Seo <>
2020-07-05 20:30:11 +0900

    #1 프로젝트 생성 1. SearchViewController 생성 2. ImageContainerViewController 생성 3. SnapKit 추가

---
+++ KaKaoImageSearcher/ImageContainerViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ImageContainerViewController.swift
+//  KaKaoImageSearcher
+//
+//  Created by TaeWon Seo on 2020/07/05.
+//  Copyright © 2020 NOWEAT. All rights reserved.
+//
+
+import UIKit
+
+class ImageContainerViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ KaKaoImageSearcher/SearchViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SearchViewController.swift
+//  KaKaoImageSearcher
+//
+//  Created by TaeWon Seo on 2020/07/05.
+//  Copyright © 2020 NOWEAT. All rights reserved.
+//
+
+import UIKit
+
+class SearchViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
