a16fe83d0f89e25000f89ccfc369ff2c68a5337a
TaeWon Seo <>
2020-07-08 00:25:40 +0900

    #4 SearchVC 수정 1. restAPIKey 값 잘못되서 수정 2. URL 인스턴스 초기화하는 string 값에 한글이 들어가면 nil을 반환해서 인코딩 코드 추가 3. url 데이터 파라미터 넘기는 부분 잘못된 부분 수정
    response.stateCode == 200 성공

--- KaKaoImageSearcher/SearchViewController.swift
+++ KaKaoImageSearcher/SearchViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-let restAPIKey: String = "939583921c9ced3da5147a6ac9f4cf4a"
+let restAPIKey: String = "KakaoAK 939583921c9ced3da5147a6ac9f4cf4a"
 
 class SearchViewController: UIViewController, UISearchResultsUpdating {
     
@@ -34,19 +34,24 @@ class SearchViewController: UIViewController, UISearchResultsUpdating {
     }
     
     func requestSearch(query: String) {
-        guard let url = URL(string: "https://dapi.kakao.com/v2/search/image/data?query=\(query)/") else { return }
+        let urlString = "https://dapi.kakao.com/v2/search/image?query=" + query
+        // 검색어에 한글이 들어가면 URL 인스턴스를 초기화할 때 nil이 반환되는 것을 방지하기 위해 인코딩.
+        let encodedString = urlString.addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed)!
+        
+        guard let url = URL(string: encodedString) else { return }
+        print("URL : \(url)")
         var request = URLRequest(url: url)
         request.httpMethod = "GET"
         request.setValue(restAPIKey, forHTTPHeaderField: "Authorization")
-        let dataTask = URLSession.shared.dataTask(with: request) { (data, response, error) in
+        URLSession.shared.dataTask(with: request) { (data, response, error) in
             if error != nil {
                 print(error ?? "")
                 return
             }
             
             let response: HTTPURLResponse = response as! HTTPURLResponse
-            print(response)
-        }
+            print(response.statusCode)
+        }.resume()
     }
     
 }
