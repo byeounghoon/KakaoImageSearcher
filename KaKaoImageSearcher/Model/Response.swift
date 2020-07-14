8885de5f881acf1d70310c1b52c5c1c06985d109
Lau <lag.gh@kakaopaycorp.com>
2020-07-14 16:18:53 +0900

    수정제안

--- KaKaoImageSearcher/Model/Response.swift
+++ KaKaoImageSearcher/Model/Response.swift
@@ -7,6 +7,12 @@
 //
 
 import Foundation
+/**
+
+1. Codable의 역할을 생각해보고 아래 코드를 리펙토링 해보세요~
+2. is_end 같은 property 같은경우 Swift Style Guild에 맞지 않는 형식이에요.
+카멜케이스로 바꿔서 저장하는 방법을 공부해보시면 좋을듯 합니다.
+*/
 
 struct ImageResponse: Codable, Equatable {
     var meta: Meta
